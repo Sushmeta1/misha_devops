b1b25b606e826d5a9ccd3529ebab6069c8cfd9c8
Shweta <shwetapant1037@gmail.com>
2025-02-17 19:15:12 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <math.h>
 
@@ -11,13 +12,15 @@ int main() {
     printf("Enter an operator (+, -, *, /, s for square, r for square root): ");
     scanf(" %c", &operator);
     
+    //square of numbers
+    
     if (operator == 's') {
         result = num1 * num1;
         printf("The square of %.2lf is: %.2lf\n", num1, result);
     } else if (operator == 'r') {
         if (num1 >= 0) {
             result = sqrt(num1);
-            printf("The square root of %.2lf is: %.2lf\n", num1, result);
+            printf("The square root of %.2lf is: %.2lf\n", num1, result);   //square root 
         } else {
             printf("Error! Square root of a negative number is not allowed.\n");
             return 1;
@@ -28,17 +31,17 @@ int main() {
     
         switch (operator) {
             case '+':
-                result = num1 + num2;
+                result = num1 + num2;      //addition
                 break;
             case '-':
-                result = num1 - num2;
+                result = num1 - num2;       // subtraction
                 break;
             case '*':
-                result = num1 * num2;
+                result = num1 * num2;       //multiplication
                 break;
             case '/':
                 if (num2 != 0) {
-                    result = num1 / num2;
+                    result = num1 / num2;    //division
                 } else {
                     printf("Error! Division by zero is not allowed.\n");
                     return 1;
